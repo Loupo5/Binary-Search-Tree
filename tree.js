03dbaff072cedcf32310adad2514919412e042ae
Loupo5 <209892110+Loupo5@users.noreply.github.com>
2026-08-27 08:40:53 +0200

    Make the recursive calls work
    Biggest problem was the base case when the recursion should stop
    Also i had problems with the returning of the recursion.
    It was a pretty simple solution with just returning the current node

--- tree.js
+++ tree.js
@@ -9,6 +9,7 @@ class Node {
 
 class Tree {
     constructor(arr) {
+        this.root = null
         this.arr = this.#sort(arr)
     }
 
@@ -22,8 +23,25 @@ class Tree {
         })
         return removedDup
     }
+
+    buildTree(arr, start = 0, end = arr.length-1) {
+        if (start > end) return null
+
+        const mid = Math.floor((start + end) / 2)
+        const node = new Node(arr[mid])
+        
+        const left = this.buildTree(arr, start, mid-1)
+        node.left = left
+        const right = this.buildTree(arr, mid+1, end)
+        node.right = right
+
+        return node
+    }
+
+
 }
 
-const tree = new Tree([2, 5, 1, 3, 6, 7, 2, 5, 1])
+const tree = new Tree([2, 5, 1, 3, 6, 7, 2, 5, 1, 10, 20, 40])
 
 console.log(tree)
+console.log(tree.buildTree(tree.arr))
